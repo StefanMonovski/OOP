ab36818da014a4d657d453082cb62769a1d80fbb
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-11-01 18:54:39 +0200

    Add RandomList

---
+++ 01.Inheritance/04.RandomList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList();
+            randomList.AddRange(Console.ReadLine().Split().ToList());
+            Console.WriteLine(randomList.RandomString());
+            Console.WriteLine(string.Join(" ", randomList));
+        }
+    }
+}
---
+++ 01.Inheritance/04.RandomList/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random random = new Random();
+            int randomIndex = random.Next(0, Count - 1);
+            string removeString = this[randomIndex];
+            RemoveAt(randomIndex);
+            return removeString;
+        }
+    }
+}
