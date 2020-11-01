941729aceacea89986565320c856ff4aa59dabf1
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-11-01 20:05:32 +0200

    Add Zoo

---
+++ 02.Inheritance-Exercise/02.Zoo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Lizard lizard = new Lizard(Console.ReadLine());
+            Snake snake = new Snake(Console.ReadLine());
+            Gorilla gorilla = new Gorilla(Console.ReadLine());
+            Bear bear = new Bear(Console.ReadLine());
+            Console.WriteLine(lizard.Name);
+            Console.WriteLine(snake.Name);
+            Console.WriteLine(gorilla.Name);
+            Console.WriteLine(bear.Name);
+        }
+    }
+}
