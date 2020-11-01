df67193b74f781ecac8b94542632f8f272e43b95
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-11-01 19:17:51 +0200

    Add StackOfStrings

---
+++ 01.Inheritance/05.StackOfStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+            string input = "";
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] inputData = input.Split();
+                Stack<string> range = new Stack<string>(inputData);
+                stackOfStrings.AddRange(range);
+            }
+            while (stackOfStrings.Count > 0)
+            {
+                Console.WriteLine($"Remove: {stackOfStrings.Pop()}");
+                Console.WriteLine($"Is empty: {stackOfStrings.IsEmpty()}");
+            }
+        }
+    }
+}
---
+++ 01.Inheritance/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (Count > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public Stack<string> AddRange(Stack<string> range)
+        {
+            while (range.Count > 0)
+            {
+                Push(range.Pop());
+            }
+            return this;
+        }
+    }
+}
