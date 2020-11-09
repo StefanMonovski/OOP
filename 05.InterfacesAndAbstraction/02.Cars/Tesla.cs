593f9a1fb472b1bc27d55b743cf58022246c7e76
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-11-09 19:48:15 +0200

    Add Cars

---
+++ 05.InterfacesAndAbstraction/02.Cars/Tesla.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+            output.AppendLine($"{Color} Seat {Model} with {Battery} Batteries");
+            output.AppendLine(Start());
+            output.AppendLine(Stop());
+            return output.ToString().Trim();
+        }
+    }
+}
