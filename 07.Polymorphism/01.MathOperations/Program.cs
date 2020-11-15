40eb383728d52f649ab5e2be43c9ac6efb84bf8a
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-11-15 13:11:40 +0200

    Add MathOperations

---
+++ 07.Polymorphism/01.MathOperations/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Operations
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MathOperations mathOperations = new MathOperations();
+            Console.WriteLine(mathOperations.Add(2, 3));
+            Console.WriteLine(mathOperations.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(mathOperations.Add(2.2M, 3.3M, 5.5M));
+        }
+    }
+}
