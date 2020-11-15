89883d7bf3ff9c2b89f98f91127096fa14a97de8
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-11-15 14:03:21 +0200

    Add Animals

---
+++ 07.Polymorphism/02.Animals/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Cat("Cat", "Fish");
+            Animal dog = new Dog("Dog", "Meat");
+            Console.WriteLine(cat.ExplainSelf());
+            Console.WriteLine(dog.ExplainSelf());
+        }
+    }
+}
