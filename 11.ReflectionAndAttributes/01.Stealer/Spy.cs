86e68b2c451710ca40198bbe936e13b564228f5d
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-11-21 21:37:31 +0200

    Add Stealer

---
+++ 11.ReflectionAndAttributes/01.Stealer/Spy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fieldNames)
+        {
+            Type classType = Type.GetType(className);
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            Object classInstance = Activator.CreateInstance(classType);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Class under investigation: {classType}");
+            foreach (FieldInfo item in classFields.Where(x => fieldNames.Contains(x.Name)))
+            {
+                sb.AppendLine($"{item.Name} = {item.GetValue(classInstance)}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
