43a76e55ab1683a58b08ca0a868d7414e76afa4a
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-11-22 17:39:42 +0200

    Add CodeTracker

---
+++ 11.ReflectionAndAttributes/06.CodeTracker/Tracker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+
+            foreach (MethodInfo method in methods)
+            {
+                if (method.CustomAttributes.Any(x => x.AttributeType == typeof(AuthorAttribute)))
+                {
+                    IEnumerable<Attribute> attributes = method.GetCustomAttributes();
+                    foreach (AuthorAttribute attribute in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                    }
+                }
+            }
+        }       
+    }
+}
