adbebe86891451cabcbcc9543faccf6812eca8f2
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-12-06 20:16:02 +0200

    Add Facade

---
+++ 17.DesignPatterns/02.Facade/CarBuilderFacade.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.Facade
+{
+    public class CarBuilderFacade
+    {
+        protected Car Car { get; set; }
+
+        public CarBuilderFacade()
+        {
+            Car = new Car();
+        }
+
+        public Car Build()
+        {
+            return Car;
+        }
+
+        public CarInfoBuilder Info()
+        {
+            return new CarInfoBuilder(Car);
+        }
+
+        public CarAddressBuilder Address()
+        {
+            return new CarAddressBuilder(Car);
+        }
+    }
+}
---
+++ 17.DesignPatterns/02.Facade/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _02.Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new CarBuilderFacade().Info().WithType("BMW").WithColor("red").WithNumberOfDoors(4).Address().InCity("Sofia").AtAddress("Center").Build();
+            Console.WriteLine(car);
+        }
+    }
+}
