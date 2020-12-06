c17d61692fe00f5ffc5cd4a9439e58f17fe0ac37
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-12-06 19:45:34 +0200

    Add Singleton

---
+++ 17.DesignPatterns/01.Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonDatabase firstSingletonDatabase = SingletonDatabase.Instance;
+            SingletonDatabase secondSingletonDatabase = SingletonDatabase.Instance;
+            SingletonDatabase thirdSingletonDatabase = SingletonDatabase.Instance;
+            SingletonDatabase fourthSingletonDatabase = SingletonDatabase.Instance;
+            Console.WriteLine(firstSingletonDatabase.GetData(0));
+            Console.WriteLine(secondSingletonDatabase.GetData(1));
+            Console.WriteLine(thirdSingletonDatabase.GetData(2));
+            Console.WriteLine(fourthSingletonDatabase.GetData(3));
+        }
+    }
+}
---
+++ 17.DesignPatterns/01.Singleton/SingletonDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace _01.Singleton
+{
+    class SingletonDatabase
+    {
+        private List<string> database;
+
+        private SingletonDatabase()
+        {
+            Console.WriteLine("Initialize singleton database");
+            database = new List<string>(){ "first", "second", "third", "fourth" };
+        }
+
+        public static SingletonDatabase Instance = new SingletonDatabase();
+
+        public string GetData(int index)
+        {
+            return database[index];
+        }
+    }
+}
